069d1490bface8fd94c3b9aa637583f3b0f6db7b
akhil272 <akhilms272@gmail.com>
2021-10-02 13:51:57 +0530

    completed tests with mix blend

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,81 @@
 function Homepage() {
   return (
-    <h1 className="text-center my-24 font-black tracking-tight text-6xl">
-      Home
-    </h1>
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <div className="mx-auto p-6">
+        <div className="flex justify-center space-x-16">
+          <img className="w-80 rounded-lg shadow-xl" src="/images/sunset.jpg" />
+          <img
+            className="w-80 rounded-lg shadow-xl"
+            src="/images/skyscrapper.jpg"
+          />
+        </div>
+        <div className="mt-16 flex justify-center space-x-10">
+          <div className="bg-red-500 relative w-80 h-80 max-w-full rounded-lg shadow-2xl overflow-hidden">
+            <img
+              className="mix-blend-overlay absolute inset-0 w-full h-full object-cover"
+              src="/images/sunset.jpg"
+            />
+            <img
+              className="mix-blend-darken absolute inset-0 w-full h-full object-cover"
+              src="/images/skyscrapper.jpg"
+            />
+          </div>
+          <div className="bg-red-500 relative w-80 h-80 max-w-full rounded-lg shadow-2xl overflow-hidden">
+            <img
+              className="mix-blend-overlay absolute inset-0 w-full h-full object-cover"
+              src="/images/sunset.jpg"
+            />
+            <img
+              className="mix-blend-lighten absolute inset-0 w-full h-full object-cover"
+              src="/images/skyscrapper.jpg"
+            />
+          </div>
+          <div className="bg-red-500 relative w-80 h-80 max-w-full rounded-lg shadow-2xl overflow-hidden">
+            <img
+              className="mix-blend-overlay absolute inset-0 w-full h-full object-cover"
+              src="/images/sunset.jpg"
+            />
+            <img
+              className="mix-blend-hard-light absolute inset-0 w-full h-full object-cover"
+              src="/images/skyscrapper.jpg"
+            />
+          </div>
+        </div>
+        {/* second row gradient blends  */}
+        <div className="mt-16 flex justify-center space-x-10">
+          <div className="bg-gradient-to-tr from-yellow-500 to-green-400 relative w-80 h-80 max-w-full rounded-lg shadow-2xl overflow-hidden">
+            <img
+              className="mix-blend-overlay absolute inset-0 w-full h-full object-cover"
+              src="/images/sunset.jpg"
+            />
+            <img
+              className="mix-blend-darken absolute inset-0 w-full h-full object-cover"
+              src="/images/skyscrapper.jpg"
+            />
+          </div>
+          <div className="bg-gradient-to-tr from-red-500 to-green-400 relative w-80 h-80 max-w-full rounded-lg shadow-2xl overflow-hidden">
+            <img
+              className="mix-blend-overlay absolute inset-0 w-full h-full object-cover"
+              src="/images/sunset.jpg"
+            />
+            <img
+              className="mix-blend-lighten absolute inset-0 w-full h-full object-cover"
+              src="/images/skyscrapper.jpg"
+            />
+          </div>
+          <div className="bg-gradient-to-tr from-blue-500 to-green-400 relative w-80 h-80 max-w-full rounded-lg shadow-2xl overflow-hidden">
+            <img
+              className="mix-blend-overlay absolute inset-0 w-full h-full object-cover"
+              src="/images/sunset.jpg"
+            />
+            <img
+              className="mix-blend-hard-light absolute inset-0 w-full h-full object-cover"
+              src="/images/skyscrapper.jpg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
